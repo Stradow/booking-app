1e9e16bb51e34d2721a5f8a98b8eb4518b4f15a1
Stradow <karydis_kostas@icloud.com>
2026-01-14 20:53:50 +0100

    basic styles

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,4 +1,22 @@
+import { Link } from "react-router-dom";
+import appLogo from "../../assets/react.svg";
+
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex justify-between bg-[#778873] min-h-[5vh] items-center px-6">
+      {
+        <Link>
+          <img src={appLogo} alt="app-logo" />
+        </Link>
+      }
+
+      <Link to="/booking">
+        {/* <button>Choose Language button for later</button> */}
+        <button className="text-white font-medium uppercase">
+          Book Service
+        </button>
+      </Link>
+    </nav>
+  );
 }
 export default Navbar;
