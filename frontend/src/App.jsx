9eda5e42a71dc24fbd71e9072eaf298e6f46a733
Alexandra Domareski <aleexandra.junges@gmail.com>
2026-01-15 18:49:43 +0100

    admin sidebar layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import BookingPage from "./Pages/BookingPage";
 import NotFoundPage from "./Pages/NotFoundPage";
+import AdminPage from "./Pages/admin/AdminPage";
+import DashboardView from "./components/admin/views/DashboardView";
+import AppointmentsView from "./components/admin/views/AppointmentsView";
+import ServicesView from "./components/admin/views/ServicesView";
+import AvailabilityView from "./components/admin/views/AvailabilityView";
+import ProfileView from "./components/admin/views/ProfileView";
+import SettingsView from "./components/admin/views/SettingsView";
 
 function App() {
   return (
@@ -10,6 +17,16 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/booking" element={<BookingPage />} />
+
+          <Route path="/admin" element={<AdminPage />}>
+            <Route index element={<DashboardView />} />
+            <Route path="appointments" element={<AppointmentsView />} />
+            <Route path="services" element={<ServicesView />} />
+            <Route path="availability" element={<AvailabilityView />} />
+            <Route path="profile" element={<ProfileView />} />
+            <Route path="settings" element={<SettingsView />} />
+          </Route>
+
           <Route path="/*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
--- frontend/src/Pages/admin/AdminPage.jsx
+++ frontend/src/Pages/admin/AdminPage.jsx
@@ -1,4 +1,14 @@
+import AdminSidebar from "../../components/admin/AdminSidebar";
+import { Outlet } from "react-router-dom";
+
 function AdminPage() {
-  return <div>AdminPage</div>;
+  return (
+    <div>
+      <AdminSidebar />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 }
 export default AdminPage;
