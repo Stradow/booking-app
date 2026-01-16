1a899d2da4d4aa89ca2c8603773091d7607e02ac
Stradow <karydis_kostas@icloud.com>
2026-01-16 08:18:05 +0100

    started styling the design of the landing folder

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/layout/Navbar";
 import HeroSection from "../components/landing/HeroSection";
 import WhyUsSection from "../components/landing/WhyUsSection";
+import AboutTherapist from "../components/landing/AboutTherapist";
 import ServicesSection from "../components/landing/ServicesSection";
 import Footer from "../components/layout/Footer";
 
@@ -10,6 +11,7 @@ function Home() {
       <Navbar />
       <HeroSection />
       <WhyUsSection />
+      <AboutTherapist />
       <ServicesSection />
       <Footer />
     </>
--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,4 +1,19 @@
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <>
+      <section className="bg-[#D8DCD6] flex justify-around h-40 items-center">
+        <div>
+          <button>About Us</button>
+          <button>Privacy Terms</button>
+          <button>Terms and Conditions</button>
+
+          <div>
+            <button>Instagram</button>
+            <button>Facebook</button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 export default Footer;
