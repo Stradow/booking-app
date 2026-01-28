2cf181e8b01d2a4696c96f1c6acf90461c87ed9b
Stradow <karydis_kostas@icloud.com>
2026-01-28 14:26:35 +0100

    fixed images paths

--- frontend/src/components/landing/HeroSection.jsx
+++ frontend/src/components/landing/HeroSection.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import heroImg from "../../assets/images/hero.png";
+import heroImg from "../../assets/images/Hero.png";
 
 function HeroSection() {
   return (
